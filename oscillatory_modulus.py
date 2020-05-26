6ac9389bf3c87a9f779a6f64e4a97d36663ea80e
Ali Shahmohammadi <ali.shahmohamadi@yahoo.com>
2020-05-26 10:53:03 -0500

    changes 5-26-20, 10:52

--- oscillatory_modulus.py
+++ oscillatory_modulus.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,6 @@ import os
 
 
 class oscilatory_modulus():
-
     """
     This class method compute the properties of the output results by the LAMMPS
     
@@ -29,11 +29,13 @@ class oscilatory_modulus():
         A function to compute shear modulus from LAMMPS softwar
 
         Args:
-        gamma: Oscillatory shear amplitude default 0.001
-        period: number of data points in each period defualt 400
+            gamma: Oscillatory shear amplitude default 0.001
+            period: number of data points in each period defualt 400
 
         output:
-        returns frequency(w), storage modulus (G1) and loss modulus G2     
+            frequency(w)
+            storage modulus(G1) 
+            loss modulus G2     
         """
 
         # Check if the file is not empty
@@ -52,22 +54,24 @@ class oscilatory_modulus():
             end = n_period; # End of the period for analysis
     
             # Initializing the average stress
-            stress_average=np.linspace(0.0,0.0,period)
+            sigma_average=np.linspace(0.0,0.0,period)
             for i in range(period):
                 for j in range(start,end):
-                    stress_average[i]+=data[j*period+i,7]
+                    sigma_average[i]+=data[j*period+i,7]
                     
-                stress_average[i]=stress_average[i]/(end-start)
+                sigma_average[i]=sigma_average[i]/(end-start)
                 
-            stress_average=stress_average-stress_average.mean()
-            a0=stress_average.max()
+            sigma_average=sigma_average-sigma_average.mean()
+            a0=sigma_average.max()
             t=np.linspace(0,2.0*math.pi,period) 
+
             # Define a function for parameter estimation
             def curve_fit_func(x,a,phi):
                 return a*np.sin(x+phi)
-                            
+
+            # Fitting the stress average data to y = sigma_max*sin(wt+phi)           
             params, params_covariance=optimize.curve_fit(curve_fit_func, t, 
-                                                         stress_average,p0=[a0,0])
+                                                         sigma_average,p0=[a0,0])
                         
             sigma_max, phi=params # Storing the resuls for sigma = sigma_max*sin(wt+pji)
 
@@ -76,5 +80,42 @@ class oscilatory_modulus():
         
             w = 1/float(freq)
         
-        return w, G1, G2
+        return w, G1, G2, sigma_average, t
 
+
+        def particle_movement(self):
+            
+            i = 0
+            ii = 0
+
+            time = np.zeros(file_count)
+
+            disp24 = np.zeros([file_count,5])
+
+            for i in range(file_count):
+
+                cnt = i*1000
+                file = pathes + "dump." + str(cnt)
+
+                try:
+                    fp = open(file)
+
+                    array = fp.readlines()
+                    ar = np.zeros([1,5])
+                    time[ii] = [float(j) for j in array[1].split()][0]
+                    for line in array:
+                        if len(line.split()) == 5:
+                            ar[0,:] = [float(j) for j in line.split()]
+                            if ar[0,0] == P[I]:
+                                disp24[ii,:]=ar[0,:]
+
+                    ii+=1
+
+                finally:
+                    fp.close()  
+
+            df_disp = pd.DataFrame(disp24, columns=["id","r","x","y","z"])
+            df_disp['time'] = time
+            df_disp = df_disp[["time","id","r","x","y","z"]]
+
+            df_disp.to_csv(PI +".csv")
